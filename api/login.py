6cc695f11ace6ec3ce6980210d9c2c445ab8a6f7
s0m3thln9 <dunlock67@gmail.com>
2024-12-03 21:22:56 +0300

    for build 19

--- api/login.py
+++ api/login.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from api.db_utils import check_user_credentials
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "https://inno-hackathon-fepo-oltor-front.vercel.app"}})
 
 @app.route('/api/login', methods=['POST'])
 def login_user():
