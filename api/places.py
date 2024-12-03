ce0392c12d82b98da8a90723f117895b1e90ff87
s0m3thln9 <dunlock67@gmail.com>
2024-12-03 20:50:12 +0300

    for build 12

--- api/places.py
+++ api/places.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from .db_utils import get_all_places
+from api.db_utils import get_all_places
 
 app = Flask(__name__)
 CORS(app)
--- api/registration.py
+++ api/registration.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from .db_utils import save_data_to_db
+from api.db_utils import save_data_to_db
 
 app = Flask(__name__)
 CORS(app)
